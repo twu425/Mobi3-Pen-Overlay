3205f0f50b5db8aa4f3de6ac7f8065b374474d14
Tony Wu <82834362+twu425@users.noreply.github.com>
2025-10-25 17:09:56 -0700

    unsaved changes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('node:path');
+const { openCustomHid } = require("./custom_hid");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
